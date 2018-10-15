a47be9230f5717aec1e19eed139f8a242a38ffc3
ricardotenorio <ricardo.heber@gmail.com>
2018-10-15 05:04:49 -0300

    Funções de texto e mudança de cor dos retângulos

--- main.lua
+++ main.lua
@@ -16,23 +16,32 @@ math.randomseed( os.time())
 -- altura e largura do tabuleiro
 local altura = display.contentHeight * .6
 local largura = display.contentWidth
+local navioCor = { .6, .4, .4 }
+local tiroCertoCor = { 0, .3, .1 }
+local tiroErradoCor = { .5, .1, .1 }
 
 local navio = { }
 local tabuleiro = { }
 local jogador = { }
 
 local jogadorUm
-local jogadorDois
+local ia
 local texto = { }
+local tamanhoNavio
+local posicaoNavio = { }
+
 
 -- fase 1 == posicionamento, 2 == batalha, 3 == reiniciar
 local fase
 -- adicionar novo navio
 -- local botaoNovoNavio = display.newImage( "image/not_a_very_good_image.png", display.contentWidth / 2, display.contentHeight * .9 )
 
+local posicionarNavioListener
 local tapListener
 local definirTexto
-local textoAcao
+local textoTopo
+local posicionarEvento
+local mudarCor
 
 ------------------- 
 -- Objetos
@@ -113,7 +122,7 @@ function tabuleiro:desenharRetangulos( )
 			self.retangulos[i][j].alpha = .5
 			self.retangulos[i][j].linha = i
 			self.retangulos[i][j].coluna = j
-			self.retangulos[i][j].ativo = true
+			self.retangulos[i][j].ocupado = false
 			self.retangulos[i][j]:addEventListener ( "tap", tapListener )
 		end
 
@@ -122,6 +131,27 @@ function tabuleiro:desenharRetangulos( )
 end
 
 
+-- Posiciona um novo navio no tabuleiro
+function jogador:posicionarNavio( pos, tam ) 
+	local novoNavio = navio:new()
+	novoNavio.posicao = pos
+	novoNavio.tamanho = tam
+
+	table.insert( self.tabuleiro.navios, novoNavio )
+	tamanhoNavio = tamanhoNavio + 1
+
+	-- percorre o grafico no eixo x e define o valor como o tamanho do navio
+	-- ex. em um navio com tamanho 5 as posições entre x1 e x2 serão preenchidas com esse valor
+	for i = novoNavio.posicao[1], novoNavio.posicao[2] do
+		mudarCor ( self.tabuleiro.retangulos[novoNavio.posicao[3]][i], navioCor ) 
+	end
+
+	-- percorre o grafico no eixo y e define o valor como o tamanho do navio
+	for i = novoNavio.posicao[3], novoNavio.posicao[4] do
+		mudarCor ( self.tabuleiro.retangulos[i][novoNavio.posicao[1]], navioCor )
+	end
+	
+end
 
 -- atira na célula selecionada
 function jogador:atirar( jogador2, linha, coluna)
@@ -156,6 +186,19 @@ end
 -- Event Listeners
 --------------------
 
+posicionarNavioListener = function ( event )
+	posicaoNavio.l1 = event.target.linha
+	posicaoNavio.c1 = event.target.coluna
+
+	if ( tamanhoNavio == 1 ) then
+		posicaoNavio.l2 = event.target.linha
+		posicaoNavio.c2 = event.target.coluna
+		jogadorUm:posicionarNavio( posicaoNavio, tamanhoNavio )
+		textoFundo( "Navio posicionado" )
+	else	
+		textoTopo( "Defina a orientação do navio")
+	end
+end
 
 
 
@@ -166,43 +209,78 @@ end
 -- Define o estado de um novo jogo
 novoJogo = function()
 	jogadorUm = jogador:new( )
-	jogadorDois = jogador:new( )
-	jogadorDois.ia = true
-	fase = 1
+	ia = jogador:new( )
+	ia.ia = true
+	tamanhoNavio = 1
+	posicaoNavio.l1 = 0
+	posicaoNavio.l2 = 0
+	posicaoNavio.c1 = 0
+	posicaoNavio.c2 = 0
 
-	definirTexto( )
-	jogadorUm.pontuacao = 1000
 	definirTexto()
 
 end
+
+
+
+-- Adiciona os listeners para o posicionamento dos navios
+-- Recebe um jogador como parâmetro
+posicionarEvento = function ( jogador )	
+	for i=1,9 do
+		for j=1,9 do
+			if ( jogador.tabuleiro.retangulos[i][j].ocupado == false ) then
+				jogador.tabuleiro.retangulos[i][j]:addEventListener( "tap", posicionarNavioListener )
+			end
+		end
+	end
+end
+
 
 -- Define o texto no topo da tela com o atual estado do jogo
 -- Verifica se já existe um texto definido(se a função já foi chamada) para evitar overlap dos textos
 definirTexto = function ( )
 	local pontuacaoUm = jogadorUm.pontuacao
-	local pontuacaoDois = jogadorDois.pontuacao
+	local pontuacaoIa = ia.pontuacao
 
 	texto.jogador = texto.jogador or display.newText( "Jogador", 50, 0)
 	texto.ia = texto.ia or display.newText( "COM", largura - 50, 0 )
 
 	if ( texto.pontuacaoUm == nil ) then
 		texto.pontuacaoUm = display.newText( pontuacaoUm, 50, 30 )
-		texto.pontuacaoDois = display.newText( pontuacaoDois, largura - 50, 30 )
+		texto.pontuacaoIa = display.newText( pontuacaoIa, largura - 50, 30 )
 	else
 		texto.pontuacaoUm.text = pontuacaoUm
-		texto.pontuacaoDois.text = pontuacaoDois
+		texto.pontuacaoIa.text = pontuacaoIa
 	end
 	
 end
 
--- Define a ação atual do jogador
-textoAcao = function ( acao )
-	if ( texto.acao == nil ) then
-		texto.acao = display.newText( acao, largura / 2, 50 )
+-- Define o texto no topo da tela
+textoTopo = function ( str )
+	if ( texto.topo == nil ) then
+		texto.topo = display.newText( str, largura / 2, 50 )
+		texto.topo.size = 15
 	else
-		texto.acao.text = acao
+		texto.topo.text = str
 	end
 end
+
+-- Define o texto no fundo da tela
+textoFundo = function ( str )
+	if ( texto.fundo == nil ) then
+		texto.fundo = display.newText( str, largura / 2, display.contentHeight - 50 )
+		texto.fundo.size = 15
+	else
+		texto.fundo.text = str
+	end
+end
+
+-- Muda a cor de um retâgulo
+-- Recebe um retâgulo e a cor
+mudarCor = function ( retangulo, cor )
+	retangulo.fill = cor
+end
+
 
 --------------------
 -- Jogo
@@ -211,12 +289,14 @@ end
 tapListener = function( event )
 
 	if(event.target.ativo) then
-		event.target.fill = { 1, 0, 0, .5 }
+		event.target.fill = navioCor
 		event.target.ativo = false
+
 	else
-		event.target.fill = { 0, 0, 1, .5 }
+		event.target.fill = tiroErradoCor
 		event.target.ativo = true
 	end
+
 end
 
 novoJogo()
@@ -224,3 +304,4 @@ definirTexto()
 local t = tabuleiro:new()
 t:desenharGrafico()
 t:desenharRetangulos()
+textoFundo('teste')
